483be91970996dfcb76c91ba1978c69cb09683e7
kookpa <141832427+kookpa@users.noreply.github.com>
2023-11-12 15:11:49 -0300

    Add files via upload

---
+++ contador de palavras/CONTADOR DE PALAVRAS/CONTADOR DE PALAVRAS/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CONTADOR_DE_PALAVRAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbTexto = new System.Windows.Forms.TextBox();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbTexto
+            // 
+            this.txbTexto.Location = new System.Drawing.Point(65, 55);
+            this.txbTexto.Name = "txbTexto";
+            this.txbTexto.Size = new System.Drawing.Size(124, 20);
+            this.txbTexto.TabIndex = 0;
+            this.txbTexto.TextChanged += new System.EventHandler(this.txbTexto_TextChanged);
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(86, 102);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(0, 13);
+            this.lblContador.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.txbTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbTexto;
+        private System.Windows.Forms.Label lblContador;
+    }
+}
+
---
+++ contador de palavras/CONTADOR DE PALAVRAS/CONTADOR DE PALAVRAS/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CONTADOR_DE_PALAVRAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txbTexto_TextChanged(object sender, EventArgs e)
+        {
+          string[] words = txbTexto.Text.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            lblContador.Text = "Numero: " + words.Length;
+        }
+    }
+}
